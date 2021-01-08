25e94c23d8d01f0fed0eef5495795dc4345fe018
lifeiluq9g4d <wangyinsuo434343@yeah.net>
2021-01-09 00:28:40 +0800

    Class Orang, Pelanggan , Petugas
    Mangga di cek lagi bang kukuh

--- Tugas Besar PBO 2017 Biro Perjalanan/src/Model/Orang.java
+++ Tugas Besar PBO 2017 Biro Perjalanan/src/Model/Orang.java
@@ -7,8 +7,61 @@ package Model;
 
 /**
  *
- * @author Kukuh Rahingga P
+ * @author Jeremi
  */
-public class Orang {
+
+public abstract class Orang{
+    private String nama;
+    private String noHp;
+    private String alamat;
+    private String jenisKelamin;
+    private String tglLahir;
+
+    public Orang(String nama, String noHp, String alamat, String jenisKelamin, String tglLahir) {
+        this.nama = nama;
+        this.noHp = noHp;
+        this.alamat = alamat;
+        this.jenisKelamin = jenisKelamin;
+        this.tglLahir = tglLahir;
+    }
     
+    public String getNama() {
+        return "Mr. " + nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoHp() {
+        return noHp;
+    }
+
+    public void setNoHp(String noHp) {
+        this.noHp = noHp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getTglLahir() {
+        return tglLahir;
+    }
+
+    public void setTglLahir(String tglLahir) {
+        this.tglLahir = tglLahir;
+    }   
 }
