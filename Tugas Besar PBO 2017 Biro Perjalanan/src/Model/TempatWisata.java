e49a1c4309e3bfcbc45f9ac2af833ef3567cb013
lifeiluq9g4d <wangyinsuo434343@yeah.net>
2021-01-14 04:00:25 +0800

    Add files via upload

--- Tugas Besar PBO 2017 Biro Perjalanan/src/Model/TempatWisata.java
+++ Tugas Besar PBO 2017 Biro Perjalanan/src/Model/TempatWisata.java
@@ -7,8 +7,51 @@ package Model;
 
 /**
  *
- * @author Kukuh Rahingga P
+ * @author Ressa Fatah
  */
 public class TempatWisata {
+    private static int countTW = 1;
     
+    private final String idTempatWisata;
+    private String namaWisata;
+    private String lokasiWisata;
+
+    // Constructor
+    public TempatWisata(String namaWisata, String lokasiWisata) {
+        idTempatWisata = "TW-" + (countTW++);
+        this.namaWisata = namaWisata;
+        this.lokasiWisata = lokasiWisata;
+    }
+
+    // Setter and Getter
+    public String getIdTempatWisata() {
+        return idTempatWisata;
+    }
+    
+    public String getNamaWisata() {
+        return namaWisata;
+    }
+
+    public void setNamaWisata(String namaWisata) {
+        this.namaWisata = namaWisata;
+    }
+
+    public String getLokasiWisata() {
+        return lokasiWisata;
+    }
+
+    public void setLokasiWisata(String lokasiWisata) {
+        this.lokasiWisata = lokasiWisata;
+    }
+
+    @Override
+    public String toString() {
+        return "idTempatWisata : " + idTempatWisata + "\n" +
+               "namaWisata     : " + namaWisata + "\n" +
+               "lokasiWisata   : " + lokasiWisata;
+    }
+    
+    public String toStringAll() {
+        return idTempatWisata + " , " + namaWisata + " , " + lokasiWisata;
+    }
 }
